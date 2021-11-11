d548e087fc09790aa624610f0bb184c0785c10ce
dungbik <q1w2e30630@gmail.com>
2021-11-11 16:30:21 +0900

    Feat: create main screen

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
 ﻿#pragma warning( disable : 4996 )
 
+#include "game_api.h"
+
+static int g_status;
+static HANDLE g_screen[2];
+static int g_cur_screen;
+static char g_game_list[4][20] = { "게임1", "게임2", "게임3", "게임4" };
+static int g_cur_game;
+
 void init()
 {
+	g_status = kStatus_Init;
+	g_cur_screen = 0;
+	g_cur_game = 0;
 
+	init_screen(g_screen, 40, 15);
+	hide_cursor(g_screen);
+
+	g_status = kStatus_Select;
+
+	system("title C-Union MiniGame");
 }
 
 void update()
@@ -12,12 +29,23 @@ void update()
 
 void render()
 {
+	clear_screen(g_screen, g_cur_screen, 40, 15);
 
-}
+	if (g_status == kStatus_Select) {
+		char title[] = "미니게임 모음";
+		print_screen(g_screen, g_cur_screen, 40 / 2 - strlen(title) / 2, 15 / 4, title);
 
-void release()
-{
+		for (int i = 0; i < 4; i++)
+		{
+			if (g_cur_game == i)
+				print_screen(g_screen, g_cur_screen, 40 / 2 - 5, 15 / 2 + i, "▶");
+			else
+				print_screen(g_screen, g_cur_screen, 40 / 2 - 5, 15 / 2 + i, "▷");
+			print_screen(g_screen, g_cur_screen, 40 / 2 - 2, 15 / 2 + i, g_game_list[i]);
+		}
+	}
 
+	flip_screen(g_screen, &g_cur_screen);
 }
 
 int main()
@@ -30,6 +58,6 @@ int main()
 		render();
 	}
 
-	release();
+	release(g_screen);
 	return 0;
 }
