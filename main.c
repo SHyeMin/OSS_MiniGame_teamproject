659b7207d4d8284c4054029c3adbba1d7766a65a
dungbik <q1w2e30630@gmail.com>
2021-11-11 16:33:22 +0900

    Feat: create exit menu

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 static int g_status;
 static HANDLE g_screen[2];
 static int g_cur_screen;
-static char g_game_list[4][20] = { "게임1", "게임2", "게임3", "게임4" };
+static char g_game_list[5][20] = { "게임1", "게임2", "게임3", "게임4", "종료하기"};
 static int g_cur_game;
 
 void init()
@@ -35,7 +35,7 @@ void render()
 		char title[] = "미니게임 모음";
 		print_screen(g_screen, g_cur_screen, 40 / 2 - strlen(title) / 2, 15 / 4, title);
 
-		for (int i = 0; i < 4; i++)
+		for (int i = 0; i < 5; i++)
 		{
 			if (g_cur_game == i)
 				print_screen(g_screen, g_cur_screen, 40 / 2 - 5, 15 / 2 + i, "▶");
