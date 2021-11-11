14a5c1ef26ffaf98fe8c81db74826813d00dbe99
dungbik <q1w2e30630@gmail.com>
2021-11-11 16:18:16 +0900

    Feat: create basic frame for main screen

---
+++ main.c
@@ -0,0 +1,35 @@
+﻿#pragma warning( disable : 4996 )
+
+void init()
+{
+
+}
+
+void update()
+{
+
+}
+
+void render()
+{
+
+}
+
+void release()
+{
+
+}
+
+int main()
+{
+	init();
+
+	while (1)
+	{
+		update();
+		render();
+	}
+
+	release();
+	return 0;
+}
